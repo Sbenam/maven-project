a6563c95562147dbd8232242c25b8f35fd88064a
Sbenam <80821053+Sbenam@users.noreply.github.com>
2021-07-02 14:56:35 +0200

    Update Greeter.java
    add javaDoc

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,15 +1,47 @@
+/**
+ * This module is about impact of the final keyword on performance
+ * <p>
+ * This module explores  if there are any performance benefits from
+ * using the final keyword in our code. This module examines the performance
+ * implications of using final on a variable, method, and class level.
+ * </p>
+ *
+ * @since 1.0
+ * @author baeldung
+ * @version 1.1
+ */
 package com.example;
-/*package java Greeter*/
+/**
+ * Description
+ *
+ * @tag1
+ * @tag2
+ */
 
 public class Greeter {
-/*class java Greeter*/
+/**
+ * Description
+ *
+ * @tag1
+ * @tag2
+ */
   
   public Greeter() {
-    /*constructeur java Greeter*/
+  /**
+ * Description
+ *
+ * @tag1
+ * @tag2
+ */
   }
 
   public final String greet(final String someone) {
-    /*methode java Greeter*/
+    /**
+ * Description
+ *
+ * @tag1
+ * @tag2
+ */
     return String.format("Hello babacar, %s!", someone);
   }
 }
