2ce85fc077e78299c4205bb9530345d30985403c
Sbenam <80821053+Sbenam@users.noreply.github.com>
2021-07-02 14:53:06 +0200

    Update Greeter.java
    add comments

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,14 +1,15 @@
 package com.example;
-
+/*package java Greeter*/
 
 public class Greeter {
-
+/*class java Greeter*/
   
   public Greeter() {
-
+    /*constructeur java Greeter*/
   }
 
   public final String greet(final String someone) {
+    /*methode java Greeter*/
     return String.format("Hello babacar, %s!", someone);
   }
 }
